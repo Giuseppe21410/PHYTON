860dbea2fd809f206d16dbe8f737333b977c7278
Giuseppe <giusepestu@gmail.com>
2025-05-15 23:35:39 +0100

    013_phyton: Finaliza el curso intermedio de phyton con el manejo de paquetes.

---
+++ APUNTES/21_Manejo_de_paquetes.py
@@ -0,0 +1,20 @@
+## Manejo de paquetes. ##
+# Será necesario para el uso de módulos no descargados. 
+
+# PIP: https://pypi.org/project/pip/ . 
+
+# Para instalar paquetes usamos el comando: "py -m pip install nombre_paquete".
+# Para actualizar pip usamos: "py -m pip install --upgrade pip".
+# Para desinstalar un paquete usamos: "py -m pip uninstall nombre_paquete".
+# Para ver la lista de paquetes instalados usamos: "py -m pip list".
+# Para buscar paquetes usamos: "py -m pip search nombre_paquete".
+# Para ver la documentación de un paquete usamos: "py -m pip show nombre_paquete".
+# Para congelar las dependencias instaladas en un archivo requirements.txt: "py -m pip freeze > requirements.txt" .
+# Para instalar paquetes desde un archivo requirements.txt: "py -m pip install -r requirements.txt" .
+# Para actualizar un paquete específico: "py -m pip install --upgrade nombre_paquete" .
+# # Para verificar si hay paquetes desactualizados: "py -m pip list --outdated" .
+#Para mostrar información detallada de pip: "py -m pip --version".
+# Para desinstalar varios paquetes a la vez: "py -m pip uninstall paquete1 paquete2 paquete3".
+# Para instalar una versión específica de un paquete: "py -m pip install nombre_paquete==1.2.3".
+
+
